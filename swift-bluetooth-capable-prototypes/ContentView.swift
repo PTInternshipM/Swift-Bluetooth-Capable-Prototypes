e7e8d6376096aadb971f43c6cad5301a196a71e9
Timo van der Haar <timo.vdhaar@mentechinnovation.eu>
2022-10-18 13:24:20 +0200

    Initial Commit

---
+++ swift-bluetooth-capable-prototypes/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  swift-bluetooth-capable-prototypes
+//
+//  Created by Timo van der Haar on 18/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift-bluetooth-capable-prototypes/swift_bluetooth_capable_prototypesApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_bluetooth_capable_prototypesApp.swift
+//  swift-bluetooth-capable-prototypes
+//
+//  Created by Timo van der Haar on 18/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct swift_bluetooth_capable_prototypesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
